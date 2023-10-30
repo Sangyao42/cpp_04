a57662e153e0326ac2216f3b167adf35c4871246
Sangyao42 <wangsangyao@hotmail.com>
2023-10-30 13:29:29 +0100

    typo

--- ex03/include/Character.hpp
+++ ex03/include/Character.hpp
@@ -6,7 +6,7 @@
 /*   By: sawang <sawang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 18:15:34 by sawang            #+#    #+#             */
-/*   Updated: 2023/10/27 18:45:08 by sawang           ###   ########.fr       */
+/*   Updated: 2023/10/30 11:44:05 by sawang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ class Character : public ICharacter
 		virtual ~Character();
 
 		virtual std::string const &getName(void) const;
+		
 		virtual void equip(AMateria *m);
 		virtual void unequip(int idx);
 		virtual void use(int idx, ICharacter &target);
