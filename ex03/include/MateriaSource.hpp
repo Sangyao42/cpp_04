d265e7399f0906327be667d3374cc4a6a9970b19
Sangyao42 <wangsangyao@hotmail.com>
2023-10-30 13:27:56 +0100

    Added[ex03]: class MateriaSource: private attribute

--- ex03/include/MateriaSource.hpp
+++ ex03/include/MateriaSource.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   MateraiSouce.hpp                                   :+:      :+:    :+:   */
+/*   MateriaSource.hpp                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sawang <sawang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 19:56:11 by sawang            #+#    #+#             */
-/*   Updated: 2023/10/27 20:33:09 by sawang           ###   ########.fr       */
+/*   Updated: 2023/10/30 12:14:36 by sawang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,19 @@
 # include "IMateriaSource.hpp"
 # include <iostream>
 
-class MateraiSource : public IMateriaSource
+class MateriaSource : public IMateriaSource
 {
 	public:
-		MateraiSource();
-		MateraiSource(const MateraiSource &copy);
-		MateraiSource &operator=(const MateraiSource &rhs);
-		virtual ~MateraiSource();
+		MateriaSource();
+		MateriaSource(const MateriaSource &copy);
+		MateriaSource &operator=(const MateriaSource &rhs);
+		virtual ~MateriaSource();
 
 		virtual void learnMateria(AMateria *m);
 		virtual AMateria *createMateria(std::string const &type);
+
+	private:
+		AMateria *_materia[4];
 };
 
 #endif
